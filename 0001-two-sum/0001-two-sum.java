8c8665aa2c90ce7167617c94b47aa44e5f687054
dgesc203 <59036432+dgesc203@users.noreply.github.com>
2022-10-24 17:17:19 +0900

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,7 +3,9 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         
         for(int i = 0 ; i<nums.length;i++){
+                //containsKey - 맵에 키가 있는지 확인, 있다면 true, 없으면 false
             if(map.containsKey(nums[i])){
+                //맵에 삽입
                 return new int[]{map.get(nums[i]), i};
             }
             map.put(target - nums[i], i);
