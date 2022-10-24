5a899ca844b097fc7433426233324423d1284b7c
dgesc203 <59036432+dgesc203@users.noreply.github.com>
2022-10-24 17:45:34 +0900

    Update 0009-palindrome-number.java

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,6 +6,7 @@ class Solution {
       int reverse = 0;
         int temp =x;
         while(temp!=0){
+                                    //나누기 10을 하면 끝자리를 얻을 수 있음
             reverse = reverse*10 + temp%10;
             temp /= 10;
             
