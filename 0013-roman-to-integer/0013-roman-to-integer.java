3901b734cf7a30ab232be4dd12b3b56c62453369
dgesc203 <59036432+dgesc203@users.noreply.github.com>
2022-10-25 11:29:01 +0900

    Time: 15 ms (23.98%), Space: 46.1 MB (48.38%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int romanToInt(String s) {
+    
+        Map<Character, Integer> map=new HashMap<Character, Integer>();  
+        //바꿀 요소 맵에 저장  
+        map.put('I',1);  
+        map.put('V',5);  
+        map.put('X',10);  
+        map.put('L',50);  
+        map.put('C',100);  
+        map.put('D',500);  
+        map.put('M',1000);    
+        s = s.replace("IV","IIII");  
+        s = s.replace("IX","VIIII");  
+        s = s.replace("XL","XXXX");  
+        s = s.replace("XC","LXXXX");  
+        s = s.replace("CD","CCCC");  
+        s = s.replace("CM","DCCCC");  
+        int number = 0;  
+    //배열 반복   
+        for (int i = 0; i < s.length(); i++)  
+        {  
+    //각각의 배열을 number에 치환하여 추가
+            number = number + (map.get(s.charAt(i)));  
+       
+       
+        }   
+         return number; 
+    }
+}
